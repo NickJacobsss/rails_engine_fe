93140a5b90581a4229364decc8dc3fea755c9765
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-20 14:21:04 -0500

    removed save open page

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'Merchant Items show spec' do
 
   it "shows all items for merchant" do
     visit '/merchants/1/items'
-    save_and_open_page
     expect(page).to have_content("Item Nemo Facere")
   end
 
